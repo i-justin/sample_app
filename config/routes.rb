3b951c665f6e8135182ab887f09cc631a76793ba
Justin Vargas <jeff99si@hotmail.com>
2011-04-08 14:12:45 -0400

    Added a users controller and layout link test

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
   
-  get "pages/about"
-  
-  get "pages/help"
+ 
 
+  root :to => "pages#home"
+  
+  match '/contact', :to => "pages#contact"
+  
+  match '/', :to => "pages#home"
+  
+  match '/help', :to => "pages#help"
+  
+  match '/about', :to => "pages#about"
+  
+  match '/signup', :to => "users#new"
+  
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
