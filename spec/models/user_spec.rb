3cbdd39d47e555c4cd15cecd673dd1718aea45bc
Justin Vargas <jeff99si@hotmail.com>
2011-04-08 16:46:48 -0400

    mu

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,9 @@ describe User do
 		User.create!(:name=>"Example User", :email=>"user@example.com")
 	end 
  
-  
+   
+	it "should require a name" do
+		no_name_user=User.new(:email=>"user@example.com")
+		no_name_user.should_not be_valid	
+	end  
 end
