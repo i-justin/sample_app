a89198f21fefbd0f732498e0651653b160fc8618
Justin Vargas <jeff99si@hotmail.com>
2011-04-12 14:32:32 -0400

    Added sign-in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
   
   def create
+     user=User.authenticate(params[:session][:email],params[:session][:password])
+     if user
+  	     #do sign in
+  	  else
+  	  	  @title="Sign in"
+  	  	  flash.now[:error]="Invalid email/password combination"
+  	     render 'new'
+  	  end
   end
   
   def destroy
