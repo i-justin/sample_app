ff44ff6b6815de3ee506a8c8df12c0151a0214d5
Justin Vargas <jeff99si@hotmail.com>
2011-04-19 12:59:58 -0400

    Access control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only=>[:edit, :update]
-
+  before_filter :correct_user, :only=>[:edit, :update]
   def new
    @user=User.new
   	@title="Sign Up"
@@ -46,6 +46,11 @@ class UsersController < ApplicationController
   	
   	def authenticate
   		deny_access unless signed_in?
+   end
+   
+   def correct_user
+   	@user=User.find(params[:id])
+   	redirect_to(root_path) unless current_user?(@user)
    end
    
 end
