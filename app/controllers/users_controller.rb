ef0baa4f6bf88e0ba2b5a537fe2594e222000768
Justin Vargas <jeff99si@hotmail.com>
2011-04-19 11:55:53 -0400

    Editing and updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,19 @@ class UsersController < ApplicationController
      		render 'new'
      end     
   end
+  
+  def edit
+  		@user=User.find_by_id(params[:id])
+  		@title="Edit User"
+  end
+  
+  def update
+  	   @user=User.find(params[:id])
+		if  @user.update_attributes(params[:user])
+			redirect_to @user, :flash=>{:success=>"Profile updated."}
+		else
+			@title="Edit user"
+			render 'edit'
+	   end
+  end
 end
